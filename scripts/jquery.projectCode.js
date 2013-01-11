cf47578d6d92261d08dd9c7e8056adb1b32b7868
amodef <julien.gascard@gmail.com>
2013-01-11 22:04:32 +0100

    New files added

---
+++ scripts/jquery.projectCode.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+	// Parallax effect
+	$('#nav').localScroll(900);
+	
+	//$('.intro_f(loat').parallax("50%", 0.1);
+	//$('.intro_glass').parallax("50%", 0.2);
+	//$('#tr3').parallax("50%", 0.1);
+	//$('#tr4').parallax("50%", 0.1);
+	
+	$('#intro .container').parallax("50%", 0.1);
+	$('#tr1').parallax("50%", 0.3);
+	
+	$('#tr2').parallax("50%", 0.3);
+	//$('#tr3').parallax("50%", 0.3);
+	//$('#tr4').parallax("50%", 0.3);
+	
+	
+	// Slider effect
+	$('#slides').slides({
+		preload: true,
+		generateNextPrev: false,
+		generatePagination: false,
+		paginationClass: 'slides_beers'
+	});
+	
+	// Externals slider links
+	$('.intro_beers a').click(function(){ 
+		var num_link = $(this).data('link');
+		$('#' + num_link).click();
+		$.scrollTo('#slides',900);
+		return false;
+	});
+	
+});

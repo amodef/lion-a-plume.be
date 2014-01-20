8b306f81770c83fbc54b5d17f99cf8cb07035459
Julien Gascard <julien@gascard.eu>
2014-01-20 23:00:17 +0100

    Add PHPMailer sending information - use OVH SSL SMTP

--- inc/process.php
+++ inc/process.php
@@ -19,11 +19,45 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 	}
 
 	if(!isset($error_message) && $_POST["address"] != ""){
-			$error_message = "Die Robot, die!";
+		$error_message = "Die Robot, die!";
+	}
+
+	require_once("inc/phpmailer/class.phpmailer.php");
+
+	$mail = new PHPMailer();
+
+	if (!isset($error_message) && $mail->ValidateAddress($email)){
+		$error_message ="You must specify a valid email address.";
+	}
+
+	$body = "";
+	$body .= "<b>Name :</b> " . $name . "<br>";
+	$body .= "<b>Email :</b> " . $email . "<br>";
+	if ($_POST["tel"] != ""){
+		$body .= "<b>Tel. :</b>" . $tel . "<br>";
+	}
+	$body .= "<p style=\"padding-top: 30px;\">" . $message . "</p>";
+
+	$mail->IsSMTP();
+	$mail->SMTPAuth = true;
+	$mail->SMTPSecure = "ssl";
+	$mail->Host = "ssl0.ovh.net";
+	$mail->Port = 465;
+	$mail->Username = "postmaster@lion-a-plume.be";
+	$mail->Password = "7gXanFyx";
+
+	$mail->SetFrom($email,$name);
+	$address = "info@lion-a-plume.be";
+	$mail->AddAddress($address, "Brasserie du Lion à Plume");
+	$mail->Subject    = "Message en provenance du site internet de la part de " . $name;
+	$mail->MsgHTML($body);
+
+	if(!$mail->Send()) {
+	  $error_message = "There was a problem sending the email: " . $mail->ErrorInfo;
 	}
 
 	header("Location: index.php?status=thanks#conta");
 	exit;
-}
+	}
 
 ?>

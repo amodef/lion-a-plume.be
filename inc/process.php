94ba2049905762b6599cfb5e74a8abef36d7f716
Julien Gascard <julien@gascard.eu>
2015-09-14 02:27:09 +0200

    Light refactoring

--- inc/process.php
+++ inc/process.php
@@ -3,87 +3,87 @@
 if($_SERVER["REQUEST_METHOD"] == "POST"){
 
 	
-	// reCAPTCHA validation
+	// reCaptcha libraries
 	require_once('inc/recaptcha/autoload.php');
 
+	// create new reCaptcha object
 	$siteKey= '6LfXyQwTAAAAADxebUrU0RaLiaGg8B6NY5n3s0RY';
 	$secret = '6LfXyQwTAAAAANcrHgV7PmzAJJMlNgHjrsNcGU-s';
 	$recaptcha = new \ReCaptcha\ReCaptcha($secret);
 	
+	// parse form response to variables
+	$name = trim($_POST["full_name"]);
+	$email = trim($_POST["email"]);
+	$tel = trim($_POST["tel"]);
+	$message = trim($_POST["message"]);
+	
+	// verify reCaptcha
 	$gRecaptchaResponse = trim($_POST['g-recaptcha-response']);
+	
 	$remoteIp = $_SERVER['REMOTE_ADDR'];
-
 	$resp = $recaptcha->verify($gRecaptchaResponse, $remoteIp);
 	
-	if ($resp->isSuccess()) {
+	if (!$resp->isSuccess()) {
+		$error_message = _('Are you human?');
+	}
 
-		$name = trim($_POST["full_name"]);
-		$email = trim($_POST["email"]);
-		$tel = trim($_POST["tel"]);
-		$message = trim($_POST["message"]);
+	if(!isset($error_message) and $name == "" || $email == "" || $message == "") {
+		$error_message = _('You must specify your email address, your name and your message.');
+	}
 
-		if($name == "" or $email == "" or $message == ""){
-			$error_message = _('You must specify your email address, your name and your message.');
-		}
-
-		if(!isset($error_message)){
-			foreach ($_POST as $value) {
-				if (stripos($value, 'Content-Type:') !== FALSE) {
-					$error_message = _('There was a problem with the information you entered.');
-				}
+	if(!isset($error_message)) {
+		foreach ($_POST as $value) {
+			if (stripos($value, 'Content-Type:') !== FALSE) {
+				$error_message = _('There was a problem with the information you entered.');
 			}
 		}
+	}
 
-		require_once("inc/phpmailer/class.phpmailer.php");
+	require_once("inc/phpmailer/class.phpmailer.php");
 
-		$mail = new PHPMailer();
+	$mail = new PHPMailer();
 
-		if (!isset($error_message) && !$mail->ValidateAddress($email)){
-			$error_message = _('You must specify a valid email address.');
+	if (!isset($error_message) && !$mail->ValidateAddress($email)) {
+		$error_message = _('You must specify a valid email address.');
+	}
+
+	if (!isset($error_message)) {
+		$body = "
+			<!doctype html>
+			<html>
+			<head>
+				<meta charset=\"utf-8\">
+			</head>
+			<body>";
+		$body .= "<b>Name :</b> " . $name . "<br>";
+		$body .= "<b>Email :</b> " . $email . "<br>";
+		if ($_POST["tel"] != ""){
+			$body .= "<b>Tel. : </b>" . $tel . "<br>";
 		}
+		$body .= "<p style=\"padding-top: 15px;\">" . $message . "</p></body></html>";
 
-		if (!isset($error_message)) {
-			$body = "
-				<!doctype html>
-				<html>
-				<head>
-					<meta charset=\"utf-8\">
-				</head>
-				<body>";
-			$body .= "<b>Name :</b> " . $name . "<br>";
-			$body .= "<b>Email :</b> " . $email . "<br>";
-			if ($_POST["tel"] != ""){
-				$body .= "<b>Tel. : </b>" . $tel . "<br>";
-			}
-			$body .= "<p style=\"padding-top: 15px;\">" . $message . "</p></body></html>";
+		$mail->IsSMTP();
+		$mail->SMTPAuth = true;
+		$mail->SMTPSecure = "ssl";
+		$mail->Host = "ssl0.ovh.net";
+		$mail->Port = 465;
+		$mail->Username = "info@lion-a-plume.be";
+		$mail->Password = "M3rc@t0r";
 
-			$mail->IsSMTP();
-			$mail->SMTPAuth = true;
-			$mail->SMTPSecure = "ssl";
-			$mail->Host = "ssl0.ovh.net";
-			$mail->Port = 465;
-			$mail->Username = "info@lion-a-plume.be";
-			$mail->Password = "M3rc@t0r";
+		$mail->SetFrom($email,$name);
+		$address = "info@lion-a-plume.be";
+		$mail->AddAddress($address, "Brasserie du Lion à Plume");
+		$mail->Subject    = "Message de " . $name . " en provenance du site web";
+		$mail->CharSet = 'utf-8';
+		$mail->MsgHTML($body);
 
-			$mail->SetFrom($email,$name);
-			$address = "info@lion-a-plume.be";
-			$mail->AddAddress($address, "Brasserie du Lion à Plume");
-			$mail->Subject    = "Message de " . $name . " en provenance du site web";
-			$mail->CharSet = 'utf-8';
-			$mail->MsgHTML($body);
-
-			if($mail->Send()) {
-				
-				header("Location: /$locale/thanks/#conta");
-				exit;
-			} else {
-			  $error_message = sprintf(_('There was a problem sending the email: %s'), $mail->ErrorInfo);
-			}
+		if($mail->Send()) {
+			
+			header("Location: /$locale/thanks/#conta");
+			exit;
+		} else {
+		  $error_message = sprintf(_('There was a problem sending the email: %s'), $mail->ErrorInfo);
 		}
-
-	// Failing reCAPTCHA
-	} else {
-    	$error_message = _('Are you human?');
 	}
 }
 

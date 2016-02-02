ebc6dee2705b54a1c32711cfc688698cea4017d8
Julien Gascard <julien@gascard.eu>
2016-02-02 03:00:48 +0100

    Add .po files to repo

---
+++ env.php
@@ -0,0 +1,10 @@
+<?php
+
+$env = [
+    'recaptcha_site_key' => '6LenHBcTAAAAAIzNxTCMa0dpUOzWs1ubI-cjuW0l',
+    'recaptcha_private_key' => '6LenHBcTAAAAAOgwRouzo5VPnnJBdW6-vA0LJtRw',
+    'mail_host' => 'ssl0.ovh.net',
+    'mail_port' => '465',
+    'mail_username' => 'info@lion-a-plume.be',
+    'mail_password' => 'bienfraichejaimelabiere',
+];
